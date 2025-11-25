5cdf49d7f9888f10bb673e7157e28ab17253fcda
sohaibshaikh-dev <sohaibshaikh186@gmail.com>
2025-11-25 23:02:44 +0530

    Add blog, testimonials, footer and language dropdown
    Introduced new sections for testimonials, blog posts, and a styled footer in index.html with supporting images. Added a language dropdown feature with toggle logic in script.js and corresponding styles in style.css. Updated layout and design for improved user experience and visual appeal.

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleDropdown() {
+    let drop = document.getElementById("dropdown");
+
+    if (drop.style.display === "block") {
+        drop.style.display = "none";
+    } else {
+        drop.style.display = "block";
+    }
+}
